388b6636ab13ff868db2fd4d3166190b640074df
jess-ar <jessica18_24@hotmail.com>
2025-04-01 19:34:45 +0200

    feat: update favicon, add GA4 tracking events, and include demo button for Pair Connect

--- src/components/header/Shadow.jsx
+++ src/components/header/Shadow.jsx
@@ -1,25 +1,41 @@
 import sombra from '@/assets/icons/sombra.svg';
 
 const Shadow = ({ onClick }) => {
-    return (
-        <div className="relative banner-sombra w-52 h-52 animate-float">
-            <div
-                className="group relative w-full h-full cursor-pointer"
-                onClick={onClick}
-            >
-                <img
-                    className="w-full h-full"
-                    src={sombra}
-                    alt="Una sombra flotante decorativa, un Sincorazón de Kingdom Hearts"
-                />
-                <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 text-white text-center bg-black bg-opacity-75 px-4 md:px-6 py-2 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 w-52 md:w-52 lg:w-52">
-                    <p className="text-sm md:text-base font-medium">
-                        Click to discover the meaning behind this portfolio.
-                    </p>
-                </div>
-            </div>
+
+  const trackClick = ({ action, category, label }) => {
+    window.gtag?.("event", action, {
+      event_category: category,
+      event_label: label,
+    });
+  };
+
+  return (
+    <div className="relative banner-sombra w-52 h-52 animate-float">
+      <div
+        className="group relative w-full h-full cursor-pointer"
+        onClick={() => {
+          trackClick({
+            action: "click_shadow",
+            category: "Interactive Element",
+            label: "Floating Heartless",
+          });
+          onClick();
+        }}
+        
+      >
+        <img
+          className="w-full h-full"
+          src={sombra}
+          alt="A decorative floating shadow, a Heartless from Kingdom Hearts"
+        />
+        <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 text-white text-center bg-black bg-opacity-75 px-4 md:px-6 py-2 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 w-52 md:w-52 lg:w-52">
+          <p className="text-sm md:text-base font-medium">
+            Click to discover the meaning behind this portfolio.
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Shadow;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -30,24 +30,44 @@ function Navbar() {
     setShowPopup(false);
   };
 
-  const handleNavClick = (soundPath) => {
+  const handleNavClick = (soundPath, section) => {
+    trackClick({
+      action: "click_nav_link",
+      category: "Navbar",
+      label: section,
+    });
     setIsNavOpen(false);
     playSound(soundPath, isMuted);
+  };
+
+  const trackClick = ({ action, category, label }) => {
+    window.gtag?.("event", action, {
+      event_category: category,
+      event_label: label,
+    });
   };
 
   return (
     <header role="banner">
       <div
-        className={`navbarcon fixed top-0 left-0 w-full z-40 text-primary  shadow-[0px_4px_10px_rgba(255,255,255,0.2)] transition-colors duration-75 ${
-          isScrolled ? "bg-[#0B1223]" : "bg-transparent"
-        }`}
+        className={`navbarcon fixed top-0 left-0 w-full z-40 text-primary  shadow-[0px_4px_10px_rgba(255,255,255,0.2)] transition-colors duration-75 ${isScrolled ? "bg-[#0B1223]" : "bg-transparent"
+          }`}
         aria-label="Main Navigation"
       >
         <div className="w-full max-w-screen-lg sm:p-6 mx-auto flex justify-between items-center ">
-          <a href="#home" aria-label="Back to home">
+          <AnchorLink
+            href="#home"
+            aria-label="Back to home"
+            onClick={() =>
+              trackClick({
+                action: "click_logo",
+                category: "Navbar",
+                label: "Logo to Home",
+              })
+            }
+          >
             <img src={logo} alt="Logo Jessica Arroyo" className="h-12" />
-          </a>
-
+          </AnchorLink>
           <div className="flex items-center gap-2 lg:hidden relative">
             {showPopup && (
               <SoundPopup
@@ -55,20 +75,17 @@ function Navbar() {
                 className="absolute -top-16 right-0"
               />
             )}
-
             <button
               onClick={toggleSound}
               aria-label={isMuted ? "Enable sound" : "Mute sound"}
               className="w-10 h-10 flex items-center justify-center text-white text-xl border border-dark rounded-full bg-dark transition-all duration-300"
             >
               <i
-                className={`fa-solid ${
-                  isMuted ? "fa-volume-xmark" : "fa-volume-high"
-                }`}
+                className={`fa-solid ${isMuted ? "fa-volume-xmark" : "fa-volume-high"
+                  }`}
                 aria-hidden="true"
               />
             </button>
-
             <button
               onClick={() => setIsNavOpen(!isNavOpen)}
               aria-label="Toggle Navigation Menu"
@@ -77,11 +94,9 @@ function Navbar() {
               className="fa-solid fa-bars text-2xl cursor-pointer"
             />
           </div>
-
           <nav
-            className={`${
-              isNavOpen ? "flex" : "hidden"
-            } flex-col lg:flex lg:flex-row justify-center items-center gap-y-4 lg:gap-x-8 absolute lg:relative top-16 lg:top-0 left-0 w-full lg:w-auto bg-terciary lg:bg-transparent mt-6 lg:mt-0 pt-4 lg:pt-0`}
+            className={`${isNavOpen ? "flex" : "hidden"
+              } flex-col lg:flex lg:flex-row justify-center items-center gap-y-4 lg:gap-x-8 absolute lg:relative top-16 lg:top-0 left-0 w-full lg:w-auto bg-terciary lg:bg-transparent mt-6 lg:mt-0 pt-4 lg:pt-0`}
             role="navigation"
           >
             <ul
@@ -94,20 +109,22 @@ function Navbar() {
                   <AnchorLink
                     href={`#${section}`}
                     className="navbar-link"
-                    onClick={() => handleNavClick("/sounds/save.mp3")}
+                    onClick={() => handleNavClick("/sounds/save.mp3", section)}
                   >
                     {section.charAt(0).toUpperCase() + section.slice(1)}
                   </AnchorLink>
                 </li>
               ))}
             </ul>
-
             <div className="relative group">
               <button
                 onClick={() => {
-                  navigator.clipboard.writeText(
-                    "jessica.arroyo.lebron@gmail.com"
-                  );
+                  trackClick({
+                    action: "click_connect_button",
+                    category: "Navbar",
+                    label: "Let's Connect",
+                  });
+                  navigator.clipboard.writeText("jessica.arroyo.lebron@gmail.com");
                   alert("¡Correo copiado al portapapeles!");
                   handleNavClick("/sounds/item-get.mp3");
                 }}
@@ -117,7 +134,6 @@ function Navbar() {
                 Let's Connect
               </button>
             </div>
-
             <ul
               className="icon text-lg flex gap-4 lg:gap-6 mb-4 lg:mb-0 mt-4 lg:mt-0 items-center"
               aria-label="Social Media Links"
@@ -129,6 +145,13 @@ function Navbar() {
                   rel="noopener noreferrer"
                   aria-label="Visit LinkedIn Profile"
                   className="w-10 h-10 border border-primary flex items-center justify-center rounded-full hover:bg-primary hover:text-secondary transition-all duration-300"
+                  onClick={() =>
+                    trackClick({
+                      action: "click_social_link",
+                      category: "Navbar",
+                      label: "LinkedIn Profile",
+                    })
+                  }
                 >
                   <i className="fa-brands fa-linkedin" aria-hidden="true"></i>
                 </a>
@@ -139,7 +162,15 @@ function Navbar() {
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="Visit GitHub Profile"
-                  className="w-10 h-10 border border-primary flex items-center justify-center rounded-full hover:bg-primary hover:text-secondary transition-all duration-300"
+                  className="w-10 h-10 border border-primary flex items-center justify-center rounded-full hover:bg-primary 
+                    hover:text-secondary transition-all duration-300"
+                  onClick={() =>
+                    trackClick({
+                      action: "click_social_link",
+                      category: "Navbar",
+                      label: "GitHub Profile",
+                    })
+                  }
                 >
                   <i className="fa-brands fa-github" aria-hidden="true"></i>
                 </a>
@@ -156,9 +187,8 @@ function Navbar() {
                   className="w-10 h-10 flex items-center justify-center text-black text-xl border border-primary rounded-full bg-primary transition-all duration-300"
                 >
                   <i
-                    className={`fa-solid ${
-                      isMuted ? "fa-volume-xmark" : "fa-volume-high"
-                    }`}
+                    className={`fa-solid ${isMuted ? "fa-volume-xmark" : "fa-volume-high"
+                      }`}
                     aria-hidden="true"
                   />
                 </button>
